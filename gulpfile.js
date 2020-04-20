47749b017cdd2a078f63333bc8e8715833d7035f
burakalp <burakalp@klonbits.com>
2020-04-20 15:51:13 +0300

    npm init and create folders

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  // uglify = require('gulp-uglify-es').default,
+  terser = require('gulp-terser'),
+  sass = require('gulp-sass'),
+  rename = require('gulp-rename'),
+  autoprefixer = require('gulp-autoprefixer'),
+  browserSync = require('browser-sync').create(),
+  size = require('gulp-size'),
+  plumber = require('gulp-plumber'),
+  sourcemaps = require('gulp-sourcemaps'),
+  browserslist = require('browserslist'),
+  babel = require('gulp-babel');
+
+
+let sassOptions = {
+    outputStyle: 'compressed',
+  },
+  prefixerOptions = {
+    grid: true,
+    browsers: ['last 2 versions'],
+  };
+
+var functionsBrowserSync = function (done) {
+    browserSync.init({
+      server: {
+        baseDir: './src',
+      },
+      online: true,
+    });
+
+    done();
+  },
+  functionsSass = function () {
+    return gulp
+      .src('./src/scss/main.scss')
+      .pipe(plumber())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sass(sassOptions))
+      .pipe(
+        size({
+          gzip: true,
+          showFiles: true,
+        }),
+      )
+      .pipe(
+        autoprefixer({
+          grid: true,
+        }),
+      )
+      .pipe(
+        rename({
+          suffix: '.min',
+        }),
+      )
+      .pipe(sourcemaps.write('../sources'))
+      .pipe(gulp.dest('./src/css'))
+      .pipe(browserSync.stream());
+  },
+  functionsHtml = function () {
+    return gulp.src(roots.srcDir + '**/*.html').pipe(browserSync.stream());
+  },
+  functionsWatch = function () {
+    gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('./src/**/*.html', gulp.series('html'));
+  };
+  
+
+gulp.task('browser-sync', functionsBrowserSync);
+gulp.task('sass', functionsSass);
+gulp.task('html', functionsHtml);
+gulp.task('watch', functionsWatch);
+gulp.task('default', gulp.series('browser-sync', 'html', 'sass', 'watch'));
